cc1ee719534d125fbed4db77518ba33964f1645b
Tim Hughey <timhughey@mac.com>
2020-05-23 10:47:18 -0400

    Improvements to Ruth Firmware Deploy Script

--- extra/bin/deploy-firmware.sh
+++ extra/bin/deploy-firmware.sh
@@ -1,72 +1,110 @@
 #!/usr/bin/env zsh
 
-host=$(hostname -s)
+ruth_devel=${HOME}/devel/ruth
 
-function run_cmd {
-    "$@"
-    local rc=$?
-    if [ $rc -ne 0 ]; then
-        echo "error with $1" >&2
-				exit 1
-    fi
-    return $rc
-}
-
-function sudo_cmd {
-    sudo -u ruth "$@"
-    local rc=$?
-    if [ $rc -ne 0 ]; then
-        echo "error with $1" >&2
-				exit 1
-    fi
-    return $rc
-}
-
-base=${HOME}/devel/ruth
-build=${base}/build
-fw_suffixes=(bin elf)
-vsn=$(git describe)
-htdocs=/dar/www/wisslanding/htdocs/helen/firmware
-
-esp_export=${base}/esp-idf/export.sh
-if [[ -f $esp_export ]]; then
-  . ${esp_export} 1> /dev/null 2> /dev/null
-else
-  echo "Unable to source ${esp_export}, aborting."
+if [[ ! -d ${ruth_devel} ]]; then
+  echo "Sorry, ${ruth_devel} doesn't exist."
   exit 1
 fi
 
-pushd -q $base
-
+host=$(hostname -s)
 if [[ "${host}" = "jophiel" ]]; then
   git pull || exit 1
 fi
 
-if [[ -x ${IDF_PATH}/add-path.sh ]]; then
-	${IDF_PATH}/add-path.sh 1> /dev/null
+# use pushd to cd to ruth devel and save the cwd
+pushd -q ${ruth_devel}
+
+# use the ESP-IDF script to finish creating the environment
+# necessary to run idf.py
+source ./esp-idf/export.sh &>! /tmp/idf-export.log
+
+# if it failed output the captured log and exit
+if [[ ! $? ]]; then
+  cat /tmp/idf-export.log
+  rm -f /tmp/idf-export.log &> /dev/null
+  popd
+  exit $?
 fi
 
-run_cmd idf.py build
+# clean up after ourselves
+rm -f /tmp/idf-export.log &> /dev/nullPATH}/export.sh &> /dev/null
 
-# echo "deploying ruth.{bin,elf} to $htdocs"
+if [[ sdkconfig.defaults -nt sdkconfig ]]; then
+  echo "sdkconfig.defaults are new, removing sdkconfig and cleaning"
+  rm -f sdkconfig
+
+  idf.py clean &>1 /dev/null
+
+  if [[ ! $? ]]; then
+    popd -q
+    echo "idf.py clean failed, aborting"
+    exit $?
+  fi
+
+fi
+
+idf.py build
+
+if [[ ! $? ]]; then
+  popd
+  exit $?
+fi
+
+fw_suffixes=(bin elf)
+vsn=$(git describe)
+htdocs=/dar/www/wisslanding/htdocs/helen/firmware
+
+pushd build
+
 for suffix in "${fw_suffixes[@]}"; do
-  src=${build}/ruth.${suffix}
-  dest=${vsn}-ruth.${suffix}
-  latest=latest.${suffix}
+
+  if [[ -f ruth.${suffix} ]]; then
+    echo "can't find ruth.${suffix}, aborting"
+
+    popd -q +2
+    exit %?
+  fi
+
 
   if [[ "${host}" != "jophiel" ]]; then
-    run_cmd scp ${src} jophiel:${htdocs}/${dest}
-    ssh jophiel "pushd -q ${htdocs} ; rm -f ${latest}"
-    ssh jophiel "pushd -q ${htdocs} ; ln -s ./${dest} ${latest}"
+    scp ruth.${suffix} jophiel:${htdocs}
+
+    if [[ ! $? ]]; then
+      popd -q +2
+      exit $?
+    fi
+
+    ssh jophiel "pushd -q $htdocs ; \
+                 rm -f latest.${suffix} ; \
+                 ln -s ./ruth.${suffix} ; \
+                 ls -l latest.${suffix}"
+
+    if [[ ! $? ]]; then
+      popd -q +2
+      exit $?
+    fi
+
   else
     pushd -q $htdocs
-    run_cmd cp $src $dest || exit 1
+    cp ruth.${suffix} ${vsn}-ruth.${suffix}
+
+    if [[ ! $? ]]; then
+      popd -q +2
+      exit $?
+    fi
 
     # point the well known name latest-ruth.* to the new file
-    sudo_cmd rm -f $latest
-    sudo_cmd ln -s ./${dest} $latest
+    rm -f latest.${suffix}
 
-		ls -l $latest
+    if [[ ! $? ]]; then
+      popd -q +2
+      exit $?
+    fi
+
+    ln -s ./ruth.${suffix} ${vsn}-ruth.${suffix}
+
+		ls -l latest.${suffix}
 		popd -q
   fi
 done
