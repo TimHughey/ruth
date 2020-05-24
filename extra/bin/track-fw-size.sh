99866a42d17c9eeb32b0e54e9016f4948ed29fbd
Tim Hughey <timhughey@mac.com>
2020-05-24 00:51:27 -0400

    Disabled Unnecessary Components and Confirmed OTA Fix

--- extra/bin/track-fw-size.sh
+++ extra/bin/track-fw-size.sh
@@ -3,7 +3,7 @@
 base=${HOME}/devel/ruth
 extra=${base}/extra
 
-tracker=${extra}/firmware-size/tracker.txt
+
 
 pushd $base
 
@@ -14,6 +14,16 @@ source $base/esp-idf/export.sh 1>/dev/null 2>/dev/null
 idf.py size 1>/dev/null 2>/dev/null || exit 1
 
 # the second make records the clean output
+
+case $argv[1] in
+  --no-log)
+    tracker=/dev/stdout
+    ;;
+  *)
+    tracker=${extra}/firmware-size/tracker.txt
+    ;;
+esac
+
 echo ">>>" >> ${tracker}
 git log -1 1>>${tracker}
 idf.py size | grep -e Total -e DRAM -e Used -e Flash 1>>${tracker}
