0c2ce8e92b1850e8710b0392db78f68457de4995
Tim Hughey <timhughey@mac.com>
2021-06-30 14:22:42 -0400

    Refactored ruth MQTT to remove public dependency on ESP MQTT

--- components/ruth_mqtt/include/ruth_mqtt/mqtt.hpp
+++ components/ruth_mqtt/include/ruth_mqtt/mqtt.hpp
@@ -21,22 +21,12 @@
 #ifndef _ruth_mqtt_hpp
 #define _ruth_mqtt_hpp
 
-#include <cstdlib>
-#include <string_view>
-
-#include <esp_event.h>
-#include <esp_log.h>
 #include <freertos/FreeRTOS.h>
-#include <freertos/event_groups.h>
-#include <freertos/queue.h>
 #include <freertos/task.h>
-#include <mqtt_client.h>
-#include <sdkconfig.h>
 
 #include "filter/subscribe.hpp"
 #include "message/handler.hpp"
 #include "message/out.hpp"
-#include "misc/ruth_task.hpp"
 
 namespace ruth {
 
@@ -65,39 +55,34 @@ public:
   MQTT(const MQTT &) = delete;
   void operator=(const MQTT &) = delete;
 
+  void connectionClosed();
+
+  void incomingMsg(message::InWrapped msg);
   static void initAndStart(const ConnOpts &opts);
+  const ConnOpts &opts() const { return _opts; }
 
   static void registerHandler(message::Handler *handler);
   static bool send(message::Out &msg);
 
   // static void shutdown();
   static void subscribe(const filter::Subscribe &filter);
+  void subscribeAck(int msg_id);
 
   static TaskHandle_t taskHandle();
 
 private:
-  inline void brokerAck() { _broker_acks++; }
-  void connectionClosed();
-  // void core(void *data); // actual function that becomes the task main loop
-  // static void coreTask(void *task_instance);
+  // static esp_err_t eventCallback(esp_mqtt_event_handle_t event);
+  // static void eventHandler(void *args, esp_event_base_t base, int32_t id, void *data);
 
-  static esp_err_t eventCallback(esp_mqtt_event_handle_t event);
-  static void eventHandler(void *args, esp_event_base_t base, int32_t id, void *data);
-  void incomingMsg(esp_mqtt_event_t *event);
-  void subscribeAck(esp_mqtt_event_handle_t event);
+  // void subscribeAck(esp_mqtt_event_handle_t event);
 
 private:
   ConnOpts _opts;
-  // bool _run_core = true;
-  // the Ruth MQTT task has the singular responsibility of announcing the startup of this host
-  // Task_t _task = {.handle = nullptr, .data = nullptr, .priority = 1, .stackSize = 2048};
-
-  esp_mqtt_client_handle_t _connection = nullptr;
-  uint64_t _broker_acks = 0;
   bool _mqtt_ready = false;
-  esp_mqtt_connect_return_code_t _last_return_code;
+  int _subscribe_msg_id;
 
-  uint16_t _subscribe_msg_id;
+  // esp_mqtt_client_handle_t _connection = nullptr;
+  // esp_mqtt_connect_return_code_t _last_return_code;
 
   static constexpr uint32_t _max_handlers = 10;
   message::Handler *_handlers[_max_handlers];
